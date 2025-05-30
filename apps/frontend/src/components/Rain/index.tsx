9dd61868d115f62f36624c79773b1bc31d9b6f72
zhixiangyao <blueskyzx@icloud.com>
2025-05-30 17:46:52 +0800

    feat(frontend): add Rain component

---
+++ apps/frontend/src/components/Rain/index.tsx
@@ -0,0 +1,64 @@
+import { useEventListener, useExternal, useMemoizedFn } from 'ahooks'
+import React, { useEffect, useRef } from 'react'
+
+const path = 'https://yaozhixiang.top/assets/js/RaindropFX.js'
+
+interface Props {
+  background: string
+}
+
+function Rain({ background }: Props) {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const currentBackground = useRef<string>()
+  const raindropFxRef = useRef<InstanceType<typeof window.RaindropFX>>()
+  const status = useExternal(path, { js: { async: true } })
+
+  const handleResizeRain = useMemoizedFn(() => {
+    const canvas = canvasRef.current
+    const rect = canvas?.getBoundingClientRect()
+
+    if (raindropFxRef.current && rect) {
+      raindropFxRef.current.resize(rect.width, rect.height)
+    }
+  })
+
+  const handleReset = useMemoizedFn(() => {
+    raindropFxRef.current?.setBackground(void 0)
+    raindropFxRef.current?.stop()
+    raindropFxRef.current = void 0
+    currentBackground.current = void 0
+  })
+
+  const handleUpdateBackground = useMemoizedFn(() => {
+    if (background !== currentBackground.current) {
+      raindropFxRef.current?.setBackground(background)
+      currentBackground.current = background
+    }
+  })
+
+  const initRain = useMemoizedFn(() => {
+    if (!canvasRef.current)
+      return
+    const canvas = canvasRef.current
+    const rect = canvas.getBoundingClientRect()
+
+    canvas.width = rect.width
+    canvas.height = rect.height
+
+    if (!status || !window.RaindropFX)
+      return
+
+    raindropFxRef.current = new window.RaindropFX({ canvas, background })
+    raindropFxRef.current.start()
+    currentBackground.current = background
+    return () => handleReset()
+  })
+
+  useEventListener('resize', handleResizeRain)
+  useEffect(initRain, [initRain, status])
+  useEffect(handleUpdateBackground, [handleUpdateBackground, background])
+
+  return <canvas ref={canvasRef} className="fixed inset-0 w-full h-full translate-x-0" style={{ zIndex: -1 }} />
+}
+
+export { Rain }
--- apps/frontend/src/pages/auth.tsx
+++ apps/frontend/src/pages/auth.tsx
@@ -2,9 +2,10 @@ import type { FormItemProps } from 'antd'
 import type { CreateRequest, LoginRequest } from '~/fetchers'
 import { LockOutlined, UserOutlined } from '@ant-design/icons'
 import { App as AntdApp, Button, Checkbox, Form, Input, Segmented } from 'antd'
-import React, { useCallback, useState } from 'react'
-import { Navigate } from 'react-router-dom'
+import React, { useCallback, useEffect, useState } from 'react'
 
+import { Navigate } from 'react-router-dom'
+import { Rain } from '~/components/Rain'
 import { useUserStore } from '~/stores/useUserStore'
 
 type TFieldType = LoginRequest & CreateRequest
@@ -19,7 +20,9 @@ const rules = {
 function Auth() {
   const { remember, loading, token, user, handleLogin, handleCreate, handleRemember } = useUserStore()
   const { message } = AntdApp.useApp()
+  const [form] = Form.useForm<TFieldType>()
   const [authType, setAuthType] = useState<TAuthType>('Login')
+  const background = authType === 'Login' ? '/image1.png' : '/image2.png'
 
   const handleFinish = useCallback(
     async ({ username, password }: TFieldType) => {
@@ -40,24 +43,30 @@ function Auth() {
     [authType, handleCreate, handleLogin, message],
   )
 
+  const handleInjectForm = useCallback(() => {
+    form.setFieldsValue({
+      username: authType === 'Login' ? user?.username ?? '' : '',
+      password: '',
+    })
+  }, [authType, form, user?.username])
+
+  useEffect(handleInjectForm, [handleInjectForm])
+
   if (token) {
     return <Navigate replace to="/" />
   }
 
   return (
     <main className="flex h-full flex-col items-center pt-60 gap-6">
+      <Rain background={background} />
+
       <Segmented<TAuthType>
         options={['Login', 'Register']}
         value={authType}
         onChange={setAuthType}
         className="select-none"
       />
-      <Form<TFieldType>
-        name="login"
-        className="w-72"
-        onFinish={handleFinish}
-        initialValues={{ username: user?.username ?? '' }}
-      >
+      <Form<TFieldType> name="auth" form={form} className="w-72" onFinish={handleFinish}>
         <Form.Item<TFieldType> name="username" rules={rules.username}>
           <Input prefix={<UserOutlined />} placeholder="用户名" />
         </Form.Item>
@@ -67,11 +76,17 @@ function Auth() {
         </Form.Item>
 
         <Form.Item>
-          <Form.Item valuePropName="checked" noStyle>
-            <Checkbox className="select-none" checked={remember} onChange={e => handleRemember(e.target.checked)}>
-              记住我
-            </Checkbox>
-          </Form.Item>
+          {authType === 'Login' && (
+            <Form.Item valuePropName="checked" noStyle>
+              <Checkbox
+                className="select-none !text-gray-200"
+                checked={remember}
+                onChange={e => handleRemember(e.target.checked)}
+              >
+                记住我
+              </Checkbox>
+            </Form.Item>
+          )}
         </Form.Item>
 
         <Form.Item>
