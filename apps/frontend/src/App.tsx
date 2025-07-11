1905e5f305f62a4f815a234a4e93f1976a70a60d
zhixiangyao <blueskyzx@icloud.com>
2025-07-11 11:28:08 +0800

    fix(frontend): fix tailwindcss intellisense don't work

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,8 +2,7 @@ import { Router } from '~/router'
 
 import { AntApp } from './components/AntApp'
 import { AntConfigProvider } from './components/AntConfigProvider'
-
-import '~/styles/global.css'
+import './styles/app.css'
 
 function App() {
   return (
